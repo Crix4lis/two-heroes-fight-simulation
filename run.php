92edbaebf4810f7b9ec6583869d36de5acea7d4e
Michał Powała <powala.michal.4@gmail.com>
2019-07-15 23:20:24 +0200

    Update run.php script

--- run.php
+++ run.php
@@ -1,13 +1,19 @@
 <?php
 declare(strict_types=1);
 
+use Monolog\Logger;
+
 require_once 'vendor/autoload.php';
 
 //initialize "container"
+$reader = new \Emagia\Reader\GameReader();
+$gameLogger = new \Emagia\Logger\GameplayLogger(new \Monolog\Logger('gameplay'), Logger::DEBUG);
+$errorLogger = new \Emagia\Logger\ErrorLogger(new \Monolog\Logger('error'), Logger::ERROR);
+$mediatorFactory = new \Emagia\MediatorFactory($gameLogger, $reader, $errorLogger);
 $randomizer = new Emagia\Randomizer\DumbRandomizer();
 $turn = new \Emagia\TurnService();
 $unitFactory = new \Emagia\Unit\UnitFactory($randomizer);
 $resolver = new \Emagia\AttackerResolver();
-$gamePlay = new \Emagia\GamePlayService($turn, $unitFactory, $resolver);
+$gamePlay = new \Emagia\GamePlayService($turn, $unitFactory, $resolver, $mediatorFactory);
 
 $gamePlay->startBattle();
